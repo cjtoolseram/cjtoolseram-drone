26a17f6876f54155b5f388d7d1e531e69b81859f
cjtoolseram <cjtoolseram@gmail.com>
2015-10-23 16:56:43 -0400

    remove previous version

--- pkg/cjtoolseram-drone-0.1.0/manifests/service.pp
+++
@@ -1,9 +0,0 @@
-# Class: drone::service
-#
-#
-class drone::service ($config_path = '/etc/drone/drone.toml') {
-	exec { "droned --config=${config_path}":
-		path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-		#refreshonly => true,
-	}
-}
--- pkg/cjtoolseram-drone-0.1.1/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# Class: drone
-#
-#
-class drone {
-	class { '::drone::install': } ->
-	class { '::drone::config': } ->
-	class { '::drone::service': }
-}
