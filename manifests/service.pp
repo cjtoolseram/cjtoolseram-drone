88f97a9b36d4b959eb81c6d9410a39150c754f1d
cjtoolseram <cjtoolseram@gmail.com>
2015-10-24 20:55:03 -0400

    change service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,8 +2,8 @@
 #
 #
 class drone::service ($config_path = '/etc/drone/drone.toml') {
-	exec { "nohup droned --config=${config_path} &":
-		path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-		#refreshonly => true,
-	}
+  service { 'drone':
+    ensure => running,
+    flags => "--config=${config_path}",
+  }
 }
