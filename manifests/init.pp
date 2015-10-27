77e4eec59dd76cccad2eab3c6ecc52a3d16f893b
cjtoolseram <cjtoolseram@gmail.com>
2015-10-27 13:09:06 -0400

    remove config_path

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class drone ($wrapper_module_name = 'drone',
   $config_file = 'drone.toml',
-  $config_path = '/etc/drone/',
   ){
 
   class { 'drone::install': }
@@ -9,9 +8,7 @@ class drone ($wrapper_module_name = 'drone',
     wrapper_module_name => $wrapper_module_name,
     config_file         => $config_file,
   }
-  class { 'drone::service':
-    config_path => "${config_path}/${config_file}",
-  }
+  class { 'drone::service': }
 
   Class['drone::install'] -> Class['drone::config'] -> Class['drone::service']
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,18 @@
 # Class: drone::service
 #
 #
-class drone::service ($config_path) {
+class drone::service {
   
-  file {'droned':
-    ensure  => file,
-    path    => '/etc/init.d/droned',
-    mode    => '0755',
-    content => template('drone/droned.erb'),
-  }
+  #file {'droned':
+  #  ensure  => file,
+  #  path    => '/etc/init.d/droned',
+  #  mode    => '0755',
+  #  content => template('drone/droned.erb'),
+  #}
 
-  service { 'droned':
+  service { 'drone':
     ensure => running,
   }
 
-  File['droned'] -> Service['droned']
+  #File['droned'] -> Service['droned']
 }
