277a4a2a8cd2fa7639ab93713e9d9bd92a9a30b0
cjtoolseram <cjtoolseram@gmail.com>
2015-11-18 10:17:37 +1100

    fix linting issue

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,23 +8,25 @@ class drone::config ( $remote_driver,
   case $remote_driver {
     'github': { $remote_config = "https://github.com?client_id=${client_id}&client_secret=${client_secret}" }
     'bitbucket': { $remote_config = "https://bitbucket.org?client_id=${client_id}&client_secret=${client_secret}" }
-    'gogs': { $remote_config = "https://gogs.hooli.com?open=false" }
+    'gogs': { $remote_config = 'https://gogs.hooli.com?open=falser' }
     'gitlab': { $remote_config = "https://gitlab.hooli.com?client_id=${client_id}&client_secret=${client_secret}" }
     default: { fail("${remote_driver} is not supported!") }
   }
 
   file_line {'remote-driver':
-    ensure    => 'present',
-    line      => "REMOTE_DRIVER=${remote_driver}",
-    path      => '/etc/drone/dronerc',
-    match     => '^REMOTE_DRIVER\=', 
-    replace   => true,   
+    ensure  => 'present',
+    line    => "REMOTE_DRIVER=${remote_driver}",
+    path    => '/etc/drone/dronerc',
+    match   => '^REMOTE_DRIVER\=',
+    replace => true,
   }
 
   file_line {'remote-config':
-    ensure    => 'present',
-    line      => "REMOTE_CONFIG=${remote_config}",
-    path      => '/etc/drone/dronerc',
-    after     => "REMOTE_DRIVER=${remote_driver}",
-    match     => '^REMOTE_CONFIG\=', 
-    replace   => true,   
+    ensure  => 'present',
+    line    => "REMOTE_CONFIG=${remote_config}",
+    path    => '/etc/drone/dronerc',
+    after   => "REMOTE_DRIVER=${remote_driver}",
+    match   => '^REMOTE_CONFIG\=',
+    replace => true,
+  }
+}
